03d75ccc818f4a9f12116663761aec0131e8b31b
Joma Ejercito <jomaejercito@gmail.com>
2018-04-27 20:54:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
         redirect "/login"
     else
         redirect "/failure"
-    end		
+    end
 	end
 
 	get "/login" do
@@ -31,11 +31,12 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		if user
+		if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect "/success"
     else
         redirect "/failure"
-    end		
+    end
 	end
 
 	get "/success" do
